1987cb1c13893bf1c7813282ddeb6482ce4f6374
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-01-16 18:08:54 +0100

    Add method to get first 10 links and clean them.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-// Fetches html of any link and prints it out to the console
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((res) => res.text())
-  .then((text) => console.log(text));
+import * as cheerio from 'cheerio';
+
+//Fetch the HTML data from website and assign to response
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const html = await response.text();
+
+// Use cheerio to parse the HTML data
+const $ = cheerio.load(html);
+
+// Get all links in <section id="images">
+
+const allLinks = $('#images')
+  .find('div > a > img')
+  .toArray()
+  .map((element) => $(element).attr('src'));
+
+// Get only first 10 links
+const firstTenLinks = allLinks.slice(0, 10);
+
+// Remove "?width=300"
+const firstTenLinksWithoutWidth = firstTenLinks.map((element) =>
+  element.slice(0, -10),
+);
+
+console.log(firstTenLinksWithoutWidth);
