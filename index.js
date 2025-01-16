e9b7f6de0f9f1347d4e6b5f039bbb659dbb7f23f
Gregory Bondaruk <gregory.bondaruk@gmail.com>
2025-01-16 14:17:56 +0100

    Tried first method, saving for self

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Fetches html of any link and prints it out to the console
-fetch('https://memegen-link-examples-upleveled.netlify.app/')
-  .then((res) => res.text())
-  .then((text) => console.log(text));
+// await fetch('https://memegen-link-examples-upleveled.netlify.app/')
+//   .then((res) => res.text())
+//   .then((body) => console.log(body));
